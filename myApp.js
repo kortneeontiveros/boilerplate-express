191acf188a4341ff93a77bdf523f7d2098f41a5f
kortneeontiveros <kortneeontiveros@email.phoenix.edu>
2022-08-07 10:23:55 -0500

    mod

--- myApp.js
+++ myApp.js
@@ -9,6 +9,14 @@ app.get('/',function(req, res) {
     res.sendFile(__dirname + '/views/index.html');
   });
 
+  app.get('/json',function(req, res) {
+    if (process.env.MESSAGE_STYLE=='uppercase'){
+        return res.json({"message": "Hello json".toUpperCase()});
+    }
+    res.json({"message": "Hello json"});
+  });
+
+
 
 
 
