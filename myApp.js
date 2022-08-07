54dd3da1e5393d4a3b6df337d3b16d8f2159185f
kortneeontiveros <kortneeontiveros@email.phoenix.edu>
2022-08-07 10:21:58 -0500

    mo

--- myApp.js
+++ myApp.js
@@ -1,5 +1,13 @@
 let express = require('express');
 let app = express();
+const path = require('path')
+require('dotenv').config();
+
+app.use('/public',express.static(path.resolve(__dirname, 'public')))
+
+app.get('/',function(req, res) {
+    res.sendFile(__dirname + '/views/index.html');
+  })
 
 
 
