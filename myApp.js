9c6e4f953b9db4af7ba55cb55aa6ae84834dfb64
kortnee o <109509696+kortneeontiveros@users.noreply.github.com>
2022-08-08 07:44:22 +0000

    s

--- myApp.js
+++ myApp.js
@@ -1,13 +1,10 @@
 let express = require('express');
 let app = express();
-const path = require('path')
+const mySecret = process.env['MESSAGE_STYLE']
 require('dotenv').config();
 
 app.use('/public',express.static(path.resolve(__dirname, 'public')));
 
-app.get('/',function(req, res) {
-    res.sendFile(__dirname + '/views/index.html');
-  });
 
   app.get('/json',function(req, res) {
     if (process.env.MESSAGE_STYLE=='uppercase'){
